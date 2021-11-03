1bac867e8f121b51685a821103dfaab48a81c8ac
Michael <mjpj@live.com>
2021-11-03 00:35:14 -0400

    completed

---
+++ index.js
@@ -0,0 +1,21 @@
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+
+
+  function setBestCustomer() {
+    bestCustomer = `not bob`;
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+  
+  const leastFavoriteCustomer = "bob"
+  function changeLeastFavoriteCustomer(){
+     return leastFavoriteCustomer = "dani"
+   
+      
+  }
